252869a1050f181ab851622f99a20791e617b2b7
lisuccia20 <lisa.manoni2@gmail.com>
2024-01-15 12:46:46 +0100

    navbar button

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,15 +1,18 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 import "./navbar.css"
 
 export default function Navbar(){
 
     const navigate = useNavigate()
+    const [isActive, setIsActive] = useState()
     return(
         <div className="navbar">
-            <a onClick={() => {navigate("/")}}>Material UI</a>
-            <a onClick={() => {navigate("/about")}}>about</a>
-            <a onClick={() => {navigate("/products")}}>examples</a>
-            <a onClick={() => {navigate("/users")}}>users</a>
+            <button onClick={() => setIsActive(!isActive)}>᳃</button>
+            <a onClick={() => {navigate("/")}} className = {isActive?"show":"hide"}>Material UI</a>
+            <a onClick={() => {navigate("/about")}} className = {isActive?"show":"hide"}>about</a>
+            <a onClick={() => {navigate("/products")}} className = {isActive?"show":"hide"}>examples</a>
+            <a onClick={() => {navigate("/users")}} className = {isActive?"show":"hide"}>users</a>
         </div>
     )
 }
