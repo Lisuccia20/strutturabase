308c24d3114c11e452e366cdfcf72caf055338b6
lisuccia20 <lisa.manoni2@gmail.com>
2024-01-16 10:59:50 +0100

    ciao coem va

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,18 +1,18 @@
 import { useNavigate } from "react-router-dom"
 import { useState } from "react"
+import { useMediaQuery } from "react-responsive"
 import "./navbar.css"
 
 export default function Navbar(){
 
     const navigate = useNavigate()
-    const [isActive, setIsActive] = useState()
+
     return(
         <div className="navbar">
-            <button onClick={() => setIsActive(!isActive)}>᳃</button>
-            <a onClick={() => {navigate("/")}} className = {isActive?"show":"hide"}>Material UI</a>
-            <a onClick={() => {navigate("/about")}} className = {isActive?"show":"hide"}>about</a>
-            <a onClick={() => {navigate("/products")}} className = {isActive?"show":"hide"}>examples</a>
-            <a onClick={() => {navigate("/users")}} className = {isActive?"show":"hide"}>users</a>
+            <a onClick={() => {navigate("/")}} >Material UI</a>
+            <a onClick={() => {navigate("/about")}}>about</a>
+            <a onClick={() => {navigate("/products")}} >examples</a>
+            <a onClick={() => {navigate("/users")}}>users</a>
         </div>
     )
 }
